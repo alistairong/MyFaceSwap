789faaab10ce9ae1532e9da5d524466a563922e1
alistair <ongalistair@gmail.com>
2018-01-28 10:18:48 +0800

    Change code to take in coord

--- overlapImages.py
+++ overlapImages.py
@@ -1,46 +1,28 @@
 # Standard imports
 import cv2
 import numpy as np 
- 
-# Read images
-src = cv2.imread("harambe.jpg")
-dst = cv2.imread("white.jpg")
- 
- 
-# Create a rough mask around the face.
-src_mask = np.zeros(src.shape, src.dtype)
-poly = np.array([ [1,1], [1,700], [700,700], [700,1] ], np.int32)
-cv2.fillPoly(src_mask, [poly], (255, 255, 255))
- 
-# This is where the CENTER of the airplane will be placed
-width, height, channels = src.shape
-center = ((int(height / 2)), (int(width / 2)))
- 
-# Clone seamlessly.
-output = cv2.seamlessClone(src, dst, src_mask, center, cv2.NORMAL_CLONE)
- 
-# Save result
-cv2.imwrite("test.jpg", output);
+from recogniseFace import getFaceCoordinates
 
-# import cv2
-# import numpy as np
+#def overlapImage(image):
+# Read images
+src = cv2.imread("barack-obama.jpg")
+coord1 = getFaceCoordinates(src)
+dst = cv2.imread("white.jpg")
+coord2 = getFaceCoordinates(dst, returnmode = '1D')
  
-# # Read images : src image will be cloned into dst
-# im = cv2.imread("harambe.jpg")
-# obj= cv2.imread("white.jpg")
+# Create a rough mask around the source image face
+src_mask = np.zeros(src.shape, src.dtype)
+#access the array to get left, up, right, down, and make 4 coords from it in array form
+for (top_left, top_right, bottom_right, bottom_left) in coord1:
+	poly = np.array([ top_left, bottom_left, bottom_right, top_right ], np.int32)
+	cv2.fillPoly(src_mask, [poly], (255, 255, 255))
  
-# # Create an all white mask
-# mask = 255 * np.ones(obj.shape, obj.dtype)
- 
-# # The location of the center of the src in the dst
-# width, height, channels = im.shape
-# center = ((int(height / 2)), (int(width / 2)))
-# print (center)
- 
-# # Seamlessly clone src into dst and put the results in output
-# #normal_clone = cv2.seamlessClone(obj, im, mask, center, cv2.NORMAL_CLONE)
-# mixed_clone = cv2.seamlessClone(obj, im, mask, center, cv2.MIXED_CLONE)
- 
-# # Write results
-# #cv2.imwrite("test.jpg", normal_clone)
-# cv2.imwrite("test2.jpg", mixed_clone)
+# Placing cropped image to the centre of the face of destination image
+for (left, top, right, bottom) in coord2:
+	center = ((int((left + right) / 2)), (int((top + bottom) / 2)))
+	
+	# Clone the images seamlessly, stitching both together
+	output = cv2.seamlessClone(src, dst, src_mask, center, cv2.NORMAL_CLONE)
+ 	
+ 	# Save result
+	cv2.imwrite("hello.jpg", output);
